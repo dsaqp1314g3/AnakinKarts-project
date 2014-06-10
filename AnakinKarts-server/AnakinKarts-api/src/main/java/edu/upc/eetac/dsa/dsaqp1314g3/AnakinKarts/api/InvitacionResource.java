6dc6683c6584359a1859e36906388408ed7afd43
draja <raja-27@hotmail.com>
2014-06-10 12:54:59 +0200

    invitar

--- AnakinKarts-server/AnakinKarts-api/src/main/java/edu/upc/eetac/dsa/dsaqp1314g3/AnakinKarts/api/InvitacionResource.java
+++ AnakinKarts-server/AnakinKarts-api/src/main/java/edu/upc/eetac/dsa/dsaqp1314g3/AnakinKarts/api/InvitacionResource.java
@@ -16,6 +16,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.Response;
 
+import com.mysql.jdbc.Statement;
+
+import edu.upc.eetac.dsa.dsaqp1314g3.AnakinKarts.api.model.Invitacion;
 import edu.upc.eetac.dsa.dsaqp1314g3.AnakinKarts.api.model.User;
 
 
@@ -28,103 +31,48 @@ public class InvitacionResource {
 	@Path("/{username}/{idevent}/invitacion/{idinvitacion}")
 	@Consumes(MediaType.ANAKINKARTS_API_INVITACION)
 	@Produces(MediaType.ANAKINKARTS_API_INVITACION)
-	public User updateProfile(@PathParam("username") String username, @PathParam("idevent") String idevent, @PathParam("idinvitacion") String idinvitacion, User user){
+	public Invitacion Invitar(Invitacion invitacion){
 		
-		System.out.println("Dentro de invitar a evento");
-		
-		User userquery = new User();
 		Connection conn = null;
 		try {
 			conn = ds.getConnection();
 		} catch (SQLException e) {
-			throw new ServerErrorException("Could not connect to the database",
-					Response.Status.SERVICE_UNAVAILABLE);
+			System.out.println(e);
+			throw new ServerErrorException("Could not connect to the database"
+					+ e, Response.Status.SERVICE_UNAVAILABLE);
 		}
-		
-		System.out.println("BD establecida");
+		System.out.println("conectados a la base de datos");
 		PreparedStatement stmt = null;
-		//Pasamos a integer para poder decir que es null
-		Integer phone = pasarInteger(user.getNphone());
-		System.out.println("Phone-> Int: "+user.getNphone()+" Integer: "+ phone);
-		Integer portal = pasarInteger(user.getNumportal());
-		System.out.println("Portal-> Int: "+user.getNumportal()+" Integer: "+ portal);
-		Integer piso = pasarInteger(user.getPiso());
-		System.out.println("Piso-> Int: "+user.getPiso()+" Integer: "+ piso);
-		Integer puerta = pasarInteger(user.getNumpuerta());
-		System.out.println("Puerta-> Int: "+user.getNumpuerta()+" Integer: "+ puerta);
-		Integer cp = pasarInteger(user.getCp());
-		System.out.println("CP-> Int: "+user.getCp()+" Integer: "+ cp);
-		
-		
-		try{
-			String sql = buildUpdateUser();
-			System.out.println("Query escrita");
-			stmt = conn.prepareStatement(sql);
-			System.out.println("Query cargada");
-			stmt.setString(1, user.getEmail());
-			stmt.setString(2, user.getName());
-			stmt.setInt(3, phone);
-			stmt.setInt(4, phone);
-			stmt.setString(5, user.getCiudad());
-			stmt.setString(6, user.getCalle());
-			stmt.setInt(7, portal);
-			stmt.setInt(8, portal);
-			stmt.setInt(9, piso);
-			stmt.setInt(10, piso);
-			stmt.setInt(11, puerta);
-			stmt.setInt(12, puerta);
-			stmt.setInt(13, cp);
-			stmt.setInt(14, cp);
-			stmt.setString(15, username);
-			//stmt.setString(10, security.getUserPrincipal().getName());
-			System.out.println("Query completa");
-			int row = stmt.executeUpdate();
-			System.out.println("Query ejecutada");
-			if (row !=0 ) {
-				stmt.close();
-				System.out.println("se ha actualizado correctamente.");
-			}			
-			System.out.println("Creando la segunda query");
-			stmt = conn.prepareStatement(buildSelectUser());
-			System.out.println("Query cargada");
-			stmt.setString(1, username);
-			//stmt.setString(1, security.getUserPrincipal().getName());
-			System.out.println("Query 2 completa");
-			ResultSet rs = stmt.executeQuery();
-			System.out.println("Query 2 ejecutada");
+
+		try {
+			
+			String sql = buildInvitar();
+			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
 			
-			if (rs.next()) {
-				System.out.println("Miramos contestacion query");
+			
+			
+			stmt.setString(1, invitacion.getInvitado());
+			
+			stmt.setString(2, invitacion.getIdevento());
+			stmt.setString(3, invitacion.getEstado());
+			
 
-				userquery.setEmail(rs.getString("email"));
-					System.out.println("Email: "+userquery.getEmail());
-				userquery.setName(rs.getString("name"));
-					System.out.println("Name: "+userquery.getName());
-				userquery.setNphone(rs.getInt("phone"));
-					System.out.println("Phone: "+userquery.getNphone());
-				userquery.setCiudad(rs.getString("ciudad"));
-					System.out.println("Ciudad: "+userquery.getCiudad());
-				userquery.setCalle(rs.getString("calle"));
-					System.out.println("Calle: "+userquery.getCalle());
-				userquery.setPiso(rs.getInt("piso"));
-					System.out.println("Piso: "+userquery.getPiso());
-					userquery.setNumportal(rs.getInt("numero"));
-					System.out.println("Numportal: "+userquery.getNumportal());
-				userquery.setNumpuerta(rs.getInt("puerta"));
-					System.out.println("Puerta: "+userquery.getNumpuerta());
-				userquery.setCp(rs.getInt("cp"));
-					System.out.println("CP: "+userquery.getCp());
-				userquery.setUsername(rs.getString("username"));
-					System.out.println("Username: "+username);
+			System.out.println("hemos llegado aqui");
+
+			stmt.executeUpdate();
+			System.out.println("Miramos contestacion query jajaldjfla");
+			ResultSet rs = stmt.getGeneratedKeys();
+		
+			if (rs.next()) {
+				System.out.println(" invitado correctamente");
+				
 			} else {
-				throw new BadRequestException("Can't view the Review");
+				
+				System.out.println("No se ha podido invitar");
 			}
 			
-			
-			
-			
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new ServerErrorException(e.getMessage(),
 					Response.Status.INTERNAL_SERVER_ERROR);
 		} finally {
@@ -135,24 +83,17 @@ public class InvitacionResource {
 			} catch (SQLException e) {
 			}
 		}
+
+		return invitacion;
+
+	}
+
+	private String buildInvitar() {
+
 		
+		return "insert into relacion (username, eventoid, invitacion ) value (?, ?, ?) ";
 		
-		return userquery;
 	}
 	
-	private Integer pasarInteger(int numeroint){
-		
-		Integer numinteger = new Integer(numeroint);
-		return numinteger;
-	}
-
-	private String buildSelectUser() {
-		return "select email, name, phone, ciudad, calle, numero, piso , puerta, cp, username from users where username=?;";
-	}
-
-	private String buildUpdateUser() {
-		return "update users set email=ifnull(?, email), name=ifnull(?, name), phone=if(?<>0, ?, phone), ciudad=ifnull(?, ciudad), calle=ifnull(?, calle), numero=if(?<>0, ?, numero), piso=if(?<>0, ?, piso), puerta=if(?<>0, ?, puerta), cp=if(?<>0, ?, cp) where username=?;";
-	}
-
 
 }
